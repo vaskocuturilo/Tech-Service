a73c63e3561c593ad4fd8ad2a5df7c439f720156
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-01-29 19:10:34 +0300

    DV-0001: Add project.

---
+++ src/test/java/site/testengineer/techInterview/TechInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package site.testengineer.techInterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
