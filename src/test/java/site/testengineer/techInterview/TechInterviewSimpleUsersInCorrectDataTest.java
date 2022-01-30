c14d5b35b7c6645a1ad9d6e597ba5f29da97b02b
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-01-30 20:30:24 +0300

    QA-0002: Add simple automation script with incorrect data.

--- src/test/java/site/testengineer/techInterview/TechInterviewSimpleUsersInCorrectDataTest.java
+++ src/test/java/site/testengineer/techInterview/TechInterviewSimpleUsersInCorrectDataTest.java
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class TechInterviewSimpleUsersTest {
+public class TechInterviewSimpleUsersInCorrectDataTest {
 
     private static RequestSpecification spec;
 
@@ -37,10 +37,10 @@ public class TechInterviewSimpleUsersTest {
     public void testTechInterviewCheckPositiveStatusCode() {
         given().spec(spec)
                 .when()
-                .get("/1")
+                .get("/2")
                 .then()
                 .assertThat()
-                .statusCode(200);
+                .statusCode(500);
     }
 
     @Test
@@ -51,7 +51,7 @@ public class TechInterviewSimpleUsersTest {
                 .get("/1")
                 .then()
                 .assertThat()
-                .time(lessThan(1000L), TimeUnit.MILLISECONDS);
+                .time(lessThanOrEqualTo(5000L), TimeUnit.MILLISECONDS);
     }
 
     @Test
@@ -62,23 +62,23 @@ public class TechInterviewSimpleUsersTest {
                 .get("/1")
                 .then()
                 .assertThat()
-                .contentType(ContentType.JSON);
+                .contentType(not(ContentType.HTML));
     }
 
     @Test
     @Description("This is automation script for check body with array")
     public void testTechInterviewCheckBody() {
         Map<String, Object> expected = new HashMap<String, Object>();
-        expected.put("id", 1);
-        expected.put("title", "This is test title");
-        expected.put("description", "This is Test exercises");
-        expected.put("completed", false);
+        expected.put("id", 2);
+        expected.put("title1", "This is test title1");
+        expected.put("description1", "This is Test exercises1");
+        expected.put("completed", true);
         given().spec(spec)
                 .when()
                 .get("/1")
                 .then()
                 .assertThat()
-                .body("username", equalTo("Test"))
-                .body("exercisesList", Matchers.hasItem(expected));
+                .body("username", not(equalTo("Test1")))
+                .body("exercisesList", not(Matchers.hasItem(expected)));
     }
 }
