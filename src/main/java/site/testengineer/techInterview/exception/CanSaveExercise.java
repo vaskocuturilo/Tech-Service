8d22b654810213c594a3e8024159ec3e621deace
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-02-04 16:13:51 +0300

    DV-00011: Add custom exception.

---
+++ src/main/java/site/testengineer/techInterview/exception/CanSaveExercise.java
@@ -0,0 +1,8 @@
+package site.testengineer.techInterview.exception;
+
+public class CanSaveExercise extends Exception {
+
+    public CanSaveExercise(String message) {
+        super(message);
+    }
+}
