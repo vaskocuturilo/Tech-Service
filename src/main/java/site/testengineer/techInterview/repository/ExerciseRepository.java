91c58ab670e5e985362116fa20f8e8fc17c2c169
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-02-12 22:28:55 +0300

    DV-00018: Fix exercise repository.

--- src/main/java/site/testengineer/techInterview/repository/ExerciseRepository.java
+++ src/main/java/site/testengineer/techInterview/repository/ExerciseRepository.java
@@ -1,12 +1,8 @@
 package site.testengineer.techInterview.repository;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import site.testengineer.techInterview.entity.Exercise;
-import site.testengineer.techInterview.entity.User;
-
-import java.util.List;
 
 public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
-    List<Exercise> findByExercise(User user, Sort sort);
+
 }
